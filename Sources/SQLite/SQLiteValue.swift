43ee29fb9edcbe8abd42d6cecf4c37131f7369eb
Anthony Drendel <atdrendel@users.noreply.github.com>
2021-04-26 23:08:15 +0200

    Add sqliteValue convenience properties on some basic types (#28)

--- Sources/SQLite/SQLiteValue.swift
+++ Sources/SQLite/SQLiteValue.swift
@@ -43,3 +43,105 @@ extension SQLiteValue {
         return string
     }
 }
+
+extension Array where Element == UInt8 {
+    public var sqliteValue: SQLiteValue {
+        .data(Data(self))
+    }
+}
+
+extension BinaryInteger {
+    public var sqliteValue: SQLiteValue {
+        .integer(Int64(self))
+    }
+}
+
+extension Bool {
+    public var sqliteValue: SQLiteValue {
+        .integer(self ? 1 : 0)
+    }
+}
+
+extension Data {
+    public var sqliteValue: SQLiteValue {
+        .data(self)
+    }
+}
+
+extension Date {
+    public var sqliteValue: SQLiteValue {
+        .text(PreciseDateFormatter.string(from: self))
+    }
+}
+
+extension StringProtocol {
+    public var sqliteValue: SQLiteValue {
+        .text(String(self))
+    }
+}
+
+extension Optional where Wrapped == Array<UInt8> {
+    public var sqliteValue: SQLiteValue {
+        switch self {
+        case .none:
+            return .null
+        case let .some(bytes):
+            return .data(Data(bytes))
+        }
+    }
+}
+
+extension Optional where Wrapped: BinaryInteger {
+    public var sqliteValue: SQLiteValue {
+        switch self {
+        case .none:
+            return .null
+        case let .some(int):
+            return int.sqliteValue
+        }
+    }
+}
+
+extension Optional where Wrapped == Bool {
+    public var sqliteValue: SQLiteValue {
+        switch self {
+        case .none:
+            return .null
+        case let .some(bool):
+            return bool.sqliteValue
+        }
+    }
+}
+
+extension Optional where Wrapped == Data {
+    public var sqliteValue: SQLiteValue {
+        switch self {
+        case .none:
+            return .null
+        case let .some(data):
+            return .data(data)
+        }
+    }
+}
+
+extension Optional where Wrapped == Date {
+    public var sqliteValue: SQLiteValue {
+        switch self {
+        case .none:
+            return .null
+        case let .some(date):
+            return date.sqliteValue
+        }
+    }
+}
+
+extension Optional where Wrapped: StringProtocol {
+    public var sqliteValue: SQLiteValue {
+        switch self {
+        case .none:
+            return .null
+        case let .some(string):
+            return string.sqliteValue
+        }
+    }
+}
