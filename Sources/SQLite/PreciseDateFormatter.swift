ec4641223334484bfe72ba3500ac53f2ef76da1f
Anthony Drendel <me@anthonydrendel.com>
2022-11-05 00:12:52 +0100

    Replace PreciseDateFormatter implementation with PreciseISO8601DateFormatter

--- Sources/SQLite/PreciseDateFormatter.swift
+++ Sources/SQLite/PreciseDateFormatter.swift
@@ -1,29 +1,27 @@
 import Foundation
+import PreciseISO8601DateFormatter
 
 // `ISO8601DateFormatter` does not maintain nanosecond precision, which makes it
 // nearly impossible to equate encodable objects that include `Date` properties.
 // `DateFormatter` maintains nanosecond precision by storing the exact
 // bit pattern of `Date.timeIntervalSinceReferenceDate`, which is the type's
 // underlying primitive. https://developer.apple.com/documentation/foundation/nsdate
-public struct PreciseDateFormatter {
+public enum PreciseDateFormatter {
     public static func string(from date: Date) -> String {
-        let bitPattern = date.timeIntervalSinceReferenceDate.bitPattern
-        return String(bitPattern)
+        formatter.string(from: date)
     }
 
     public static func date(from string: String) -> Date? {
-        guard let bitPattern = UInt64(string) else { return nil }
-        let double = Double(bitPattern: bitPattern)
-        return Date(timeIntervalSinceReferenceDate: double)
+        formatter.date(from: string)
     }
 }
 
-extension KeyedDecodingContainer {
-    public func decodePreciseDate(forKey key: K) throws -> Date {
-        let asString = try self.decode(String.self, forKey: key)
+public extension KeyedDecodingContainer {
+    func decodePreciseDate(forKey key: K) throws -> Date {
+        let asString = try decode(String.self, forKey: key)
         guard let date = PreciseDateFormatter.date(from: asString) else {
             let context = DecodingError.Context(
-                codingPath: self.codingPath,
+                codingPath: codingPath,
                 debugDescription: "Could not parse '\(asString)' into Date."
             )
             throw Swift.DecodingError.typeMismatch(Date.self, context)
@@ -31,11 +29,11 @@ extension KeyedDecodingContainer {
         return date
     }
 
-    public func decodePreciseDateIfPresent(forKey key: K) throws -> Date? {
-        guard let asString = try self.decodeIfPresent(String.self, forKey: key) else { return nil }
+    func decodePreciseDateIfPresent(forKey key: K) throws -> Date? {
+        guard let asString = try decodeIfPresent(String.self, forKey: key) else { return nil }
         guard let date = PreciseDateFormatter.date(from: asString) else {
             let context = DecodingError.Context(
-                codingPath: self.codingPath,
+                codingPath: codingPath,
                 debugDescription: "Could not parse '\(asString)' into Date."
             )
             throw Swift.DecodingError.typeMismatch(Date.self, context)
@@ -44,13 +42,15 @@ extension KeyedDecodingContainer {
     }
 }
 
-extension KeyedEncodingContainer {
-    public mutating func encode(preciseDate: Date, forKey key: K) throws {
-        try self.encode(PreciseDateFormatter.string(from: preciseDate), forKey: key)
+public extension KeyedEncodingContainer {
+    mutating func encode(preciseDate: Date, forKey key: K) throws {
+        try encode(PreciseDateFormatter.string(from: preciseDate), forKey: key)
     }
 
-    public mutating func encodeIfPresent(preciseDate: Date?, forKey key: K) throws {
-        guard let preciseDate = preciseDate else { return }
-        try self.encodeIfPresent(PreciseDateFormatter.string(from: preciseDate), forKey: key)
+    mutating func encodeIfPresent(preciseDate: Date?, forKey key: K) throws {
+        guard let preciseDate else { return }
+        try encodeIfPresent(PreciseDateFormatter.string(from: preciseDate), forKey: key)
     }
 }
+
+private let formatter = PreciseISO8601DateFormatter()
